8a86211aa0ab81b53f88c15bbfbaf11c1372a200
heinwol <heinvvol@gmail.com>
2023-11-13 01:45:19 +0300

    we wrote query, yaaay

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io;
 
 use tree_sitter;
 use tree_sitter::Node;
+use tree_sitter::QueryMatches;
 use tree_sitter_graph;
 use tree_sitter_nix;
 
@@ -18,14 +19,30 @@ fn main() {
     let lang = tree_sitter_nix::language();
     let mut s = tree_sitter::Parser::new();
     s.set_language(lang);
-    let text = fs::read_to_string("/home/heinwol/nixos-config/home-modules/fish.nix").unwrap();
+    let text =
+        fs::read_to_string("/home/heinwol/Documents/work/projects/update-nix-fetchers/test.nix")
+            .unwrap();
+    let text_str = text.as_bytes();
     let tree = s.parse(&text, None).unwrap();
     // tree_sitter_graph
     // let mut f = io::BufWriter
-    let mut f = fs::File::create("/home/heinwol/temp/update-nix-fetchers/temp.dot").unwrap();
+    let mut f =
+        fs::File::create("/home/heinwol/Documents/work/projects/update-nix-fetchers/temp.dot")
+            .unwrap();
     tree.print_dot_graph(&f);
     let cursor = tree.walk();
-    let source: String = "lala".into();
-    let exp = move_node_by_list(&cursor.node(), &[0, 2, 1, 0, 2, 1, 0, 0, 0]).to_sexp();
-    println!("{:#?}", exp);
+    // let exp = move_node_by_list(&cursor.node(), &[0, 2, 1, 0, 2, 1, 0, 0, 0]).to_sexp();
+    let top = &cursor.node();
+
+    // that's literal "source_plugin"
+    let exp = move_node_by_list(top, &[0, 1]);
+    // let it = exp.field_name_for_child(0).unwrap();
+
+    let q = tree_sitter::Query::new(lang, "((identifier) @start (#eq? @start \"x\"))").unwrap();
+    let mut qc = tree_sitter::QueryCursor::new();
+    // qc.
+    let mtch = qc.matches(&q, exp, text_str);
+    let mtchvec: Vec<tree_sitter::QueryMatch> = mtch.collect();
+    // println!("{:#?}", top);
+    println!("{:#?}", mtchvec);
 }
